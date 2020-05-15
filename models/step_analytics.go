d5bb98870a6ba5614014223aa18be26e6a6d9d92
Krisztián Gödrei <godreikrisztian@gmail.com>
2020-05-15 16:10:19 +0200

    Add step inputs to StepAnalytics (#12)
    * Use Bitrise native Go test steps. instead of script steps.
    
    * Group BuildAnalytics and StepAnalytics struct properties, add StepInputs to StepAnalytics.
    
    * Typo fix.

--- models/step_analytics.go
+++ models/step_analytics.go
@@ -6,15 +6,19 @@ import (
 
 // StepAnalytics ...
 type StepAnalytics struct {
-	AppSlug     string        `json:"-"`
-	BuildSlug   string        `json:"-" track:"build_slug"`
-	StepID      string        `json:"step_id" track:"step_id"`
-	StepTitle   string        `json:"step_title" track:"step_title"`
-	StepVersion string        `json:"step_verion" track:"step_verion"`
-	StepSource  string        `json:"step_source" track:"step_source"`
-	Status      string        `json:"status" track:"status"`
-	StartTime   time.Time     `json:"start_time" track:"start_time"`
-	Runtime     time.Duration `json:"run_time" track:"run_time"`
+	// connect step analytics to Bitrise builds
+	AppSlug   string `json:"-"`
+	BuildSlug string `json:"-" track:"build_slug"`
+
+	StepID      string            `json:"step_id" track:"step_id"`
+	StepTitle   string            `json:"step_title" track:"step_title"`
+	StepVersion string            `json:"step_version" track:"step_version"`
+	StepSource  string            `json:"step_source" track:"step_source"`
+	StepInputs  map[string]string `json:"step_inputs" track:"step_inputs"`
+
+	Status    string        `json:"status" track:"status"`
+	StartTime time.Time     `json:"start_time" track:"start_time"`
+	Runtime   time.Duration `json:"run_time" track:"run_time"`
 }
 
 // Event ...
