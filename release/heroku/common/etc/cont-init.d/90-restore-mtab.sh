f2c635343076dc4b766eb2c69c6fa0257f628539
Olivér Falvai <oliver.falvai@bitrise.io>
2025-07-24 12:59:48 +0200

    cleanup dead code

--- release/heroku/common/etc/cont-init.d/90-restore-mtab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# fix the missing `/etc/mtab` file error
-if [[ ! -f /etc/mtab ]]; then
-    ln -sf /proc/mounts /etc/mtab
-fi
--- release/heroku/common/etc/cont-init.d/91-populate-datadog-yaml.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# inject the env name into datadog config
-sed -i 's@__APM_ENV__@'"${GO_ENV}"'@g' /etc/datadog-agent/datadog.yaml
