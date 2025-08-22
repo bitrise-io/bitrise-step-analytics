57192cb2ffad023639ba28d76033633f02b4d83e
Olivér Falvai <oliver.falvai@bitrise.io>
2025-08-22 14:06:55 +0200

    App Store Connect API metrics (#46)

--- models/track_analytics.go
+++ models/track_analytics.go
@@ -1,8 +1,22 @@
 package models
 
 type TrackEvent struct {
-	ID         string                 `json:"id"`
-	EventName  string                 `json:"event_name"`
-	Timestamp  int64                  `json:"timestamp"`
-	Properties map[string]interface{} `json:"properties"`
+	ID         string         `json:"id"`
+	EventName  string         `json:"event_name"`
+	Timestamp  int64          `json:"timestamp"`
+	Properties map[string]any `json:"properties"`
+}
+
+func (e TrackEvent) StringProp(key string) string {
+	if val, ok := e.Properties[key].(string); ok {
+		return val
+	}
+	return ""
+}
+
+func (e TrackEvent) IntProp(key string) int {
+	if val, ok := e.Properties[key].(int); ok {
+		return val
+	}
+	return 0
 }
